e054cb25db02dcfdefb73573a1f0c4de090ca17f
Flin7021 <flin7021@gmail.com>
2023-06-06 15:01:45 -0400

    database working

--- server/db/models/Dog.js
+++ server/db/models/Dog.js
@@ -1,144 +1,23 @@
 const Sequelize = require('sequelize')
 const db = require ('../db')
 
-[{
-  "id": 1,
-  "name": "Tye",
-  "image": "http://dummyimage.com/204x100.png/5fa2dd/ffffff",
-  "age": 12,
-  "breed": "European wild cat",
-  "gender": "Male"
-}, {
-  "id": 2,
-  "name": "Meggy",
-  "image": "http://dummyimage.com/169x100.png/cc0000/ffffff",
-  "age": 1,
-  "breed": "Gull, kelp",
-  "gender": "Female"
-}, {
-  "id": 3,
-  "name": "Donetta",
-  "image": "http://dummyimage.com/101x100.png/cc0000/ffffff",
-  "age": 35,
-  "breed": "Crocodile, nile",
-  "gender": "Female"
-}, {
-  "id": 4,
-  "name": "Madelin",
-  "image": "http://dummyimage.com/121x100.png/ff4444/ffffff",
-  "age": 12,
-  "breed": "Duck, blue",
-  "gender": "Female"
-}, {
-  "id": 5,
-  "name": "Elly",
-  "image": "http://dummyimage.com/191x100.png/dddddd/000000",
-  "age": 1,
-  "breed": "Rhea, gray",
-  "gender": "Female"
-}, {
-  "id": 6,
-  "name": "Anni",
-  "image": "http://dummyimage.com/113x100.png/5fa2dd/ffffff",
-  "age": 32,
-  "breed": "Giant otter",
-  "gender": "Female"
-}, {
-  "id": 7,
-  "name": "Christiano",
-  "image": "http://dummyimage.com/215x100.png/5fa2dd/ffffff",
-  "age": 34,
-  "breed": "Sea birds (unidentified)",
-  "gender": "Bigender"
-}, {
-  "id": 8,
-  "name": "Adella",
-  "image": "http://dummyimage.com/236x100.png/dddddd/000000",
-  "age": 11,
-  "breed": "Long-tailed spotted cat",
-  "gender": "Female"
-}, {
-  "id": 9,
-  "name": "Paulita",
-  "image": "http://dummyimage.com/120x100.png/dddddd/000000",
-  "age": 12,
-  "breed": "Eagle, crowned",
-  "gender": "Female"
-}, {
-  "id": 10,
-  "name": "Mathew",
-  "image": "http://dummyimage.com/172x100.png/dddddd/000000",
-  "age": 22,
-  "breed": "White-bellied sea eagle",
-  "gender": "Male"
-}, {
-  "id": 11,
-  "name": "Jaymee",
-  "image": "http://dummyimage.com/236x100.png/5fa2dd/ffffff",
-  "age": 13,
-  "breed": "Madagascar fruit bat",
-  "gender": "Female"
-}, {
-  "id": 12,
-  "name": "Iona",
-  "image": "http://dummyimage.com/187x100.png/ff4444/ffffff",
-  "age": 17,
-  "breed": "Flicker, campo",
-  "gender": "Female"
-}, {
-  "id": 13,
-  "name": "Rodolfo",
-  "image": "http://dummyimage.com/197x100.png/5fa2dd/ffffff",
-  "age": 31,
-  "breed": "Lion, steller's sea",
-  "gender": "Male"
-}, {
-  "id": 14,
-  "name": "Mervin",
-  "image": "http://dummyimage.com/151x100.png/ff4444/ffffff",
-  "age": 31,
-  "breed": "Thomson's gazelle",
-  "gender": "Male"
-}, {
-  "id": 15,
-  "name": "Padriac",
-  "image": "http://dummyimage.com/174x100.png/ff4444/ffffff",
-  "age": 12,
-  "breed": "Roseate cockatoo",
-  "gender": "Male"
-}, {
-  "id": 16,
-  "name": "Irvine",
-  "image": "http://dummyimage.com/222x100.png/ff4444/ffffff",
-  "age": 33,
-  "breed": "Tiger snake",
-  "gender": "Male"
-}, {
-  "id": 17,
-  "name": "Ailina",
-  "image": "http://dummyimage.com/127x100.png/ff4444/ffffff",
-  "age": 1,
-  "breed": "Squirrel, eurasian red",
-  "gender": "Female"
-}, {
-  "id": 18,
-  "name": "Lonny",
-  "image": "http://dummyimage.com/215x100.png/cc0000/ffffff",
-  "age": 22,
-  "breed": "Asian foreset tortoise",
-  "gender": "Male"
-}, {
-  "id": 19,
-  "name": "Lindie",
-  "image": "http://dummyimage.com/146x100.png/5fa2dd/ffffff",
-  "age": 22,
-  "breed": "Moose",
-  "gender": "Female"
-}, {
-  "id": 20,
-  "name": "Margarethe",
-  "image": "http://dummyimage.com/107x100.png/5fa2dd/ffffff",
-  "age": 22,
-  "breed": "Genet, small-spotted",
-  "gender": "Genderfluid"
-}]
+
+const Dog = db.define('dog', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING
+  },
+  sponsorFee: {
+    type: Sequelize.INTEGER
+  },
+  gender: {type: Sequelize.STRING
+  },
+  imageURL: {
+    type: Sequelize.STRING
+  }
+})
+
+module.exports = Dog
