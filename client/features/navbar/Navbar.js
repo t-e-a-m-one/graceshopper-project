e0529b1a093cba65fc6ded9ea7a59224d1dbad4b
Ashley Ware <ashley.met@gmail.com>
2023-06-06 17:06:19 -0400

    basic + navbar

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
-import { logout } from '../../app/store';
-
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { logout } from "../../app/store";
+// import "./Navbar.css";
 const Navbar = () => {
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const logoutAndRedirectHome = () => {
     dispatch(logout());
-    navigate('/login');
+    navigate("/login");
   };
-
   return (
     <div>
-      <h1>FS-App-Template</h1>
+      <h1>Fetch</h1>
       <nav>
         {isLoggedIn ? (
           <div>
@@ -27,8 +26,28 @@ const Navbar = () => {
         ) : (
           <div>
             {/* The navbar will show these links before you log in */}
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
+            <div className="header">
+              <img
+                className="header__logo"
+                src="https://picsum.photos/200/40"
+                alt="Fetch Logo"
+              />
+              <div className="header__search">
+                <input className="header__searchInput" type="text" />
+              </div>
+              <div className="header__nav">
+                <div className="header__option">
+                  <span className="header__optionLineOne">Log In</span>
+                </div>
+                <div className="header__option">
+                  <span className="header__optionLineOne">Register </span>
+                </div>
+                <div className="header__optionBasket">
+                  <span className="header__optionLineTwo header__basketCount"></span>
+                </div>
+                <div className="header__favorites"></div>
+              </div>
+            </div>
           </div>
         )}
       </nav>
@@ -36,5 +55,4 @@ const Navbar = () => {
     </div>
   );
 };
-
 export default Navbar;
