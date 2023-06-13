d0813b4b8742182d94d7cf16e2e22f4867074409
Ashley Ware <ashley.met@gmail.com>
2023-06-13 18:26:12 -0400

    final-ish css and home functionality

--- client/features/home/Home.js
+++ client/features/home/Home.js
@@ -1,29 +1,9 @@
-// // export default Homeimport React from "react";
-// import { useSelector } from "react-redux";
-// import { Link } from "react-router-dom";
-// import AllDogs from "../allDogs/AllDogs"; // Import the AllDogs component
-
-// /**
-//  * COMPONENT
-//  */
-// const Home = (props) => {
-//   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
-
-//   return (
-//     <div>
-//       <h3>Welcome!</h3>
-
-//       {isLoggedIn && <AllDogs />}
-//       {!isLoggedIn && <p>Please log in to view the dogs.</p>}
-//     </div>
-//   );
-// };
-
 // export default Home;
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import AllDogs from "../allDogs/AllDogs"; // Import the AllDogs component
+import "./Home.css";
 
 /**
  * COMPONENT
@@ -32,10 +12,29 @@ const Home = () => {
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
 
   return (
-    <div>
-      <h3>Welcome!</h3>
+    <div class="introdiv">
+      <h3 id="welcome">Welcome!</h3>
 
-      {isLoggedIn ? <AllDogs /> : <p>Please log in to view the dogs.</p>}
+      {isLoggedIn ? (
+        <AllDogs />
+      ) : (
+        <p id="mustlogin">🐶 Please log in to view our dogs! 🐶</p>
+      )}
+      <div class="card">
+        <p id="introduction">
+          Get ready to wag your tails because it's always a pawsitively
+          delightful paw-ty here at Fetch! Our tails are wagging with excitement
+          as we strive to sniff out the perfect fur-ever match between doggos
+          and their paw-some humans. But wait, there's more! You, yes you, have
+          the power to lend a helping paw and make a real difference. How, you
+          ask? By becoming a proud sponsor of our delightful doggos! Join our
+          pack today and embark on a journey filled with unconditional love and
+          endless tail-wagging adventures. So why wait? Let's unleash the joy
+          and make the world a barktastic place, one sponsorship at a time. It's
+          time to grab life by the leash and lend a paw! Sponsor a dog today and
+          experience the boundless love and tail-wagging fun that awaits you!
+        </p>
+      </div>
     </div>
   );
 };
