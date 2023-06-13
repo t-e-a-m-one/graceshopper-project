b2d96462df031c7c41fab086653c47be6ca47192
Ashley Ware <ashley.met@gmail.com>
2023-06-13 15:26:04 -0400

    admin nav fixed

--- client/app/AppRoutes.js
+++ client/app/AppRoutes.js
@@ -1,6 +1,76 @@
+// import React, { useEffect } from "react";
+// import { useSelector, useDispatch } from "react-redux";
+// import { Route, Routes, useNavigate } from "react-router-dom";
+// import AuthForm from "../features/auth/AuthForm";
+// import Home from "../features/home/Home";
+// import { me } from "./store";
+// import AllDogs from "../features/allDogs/AllDogs";
+// import SingleDog from "../features/singleDog/SingleDog";
+// import Cart from "../features/checkout/Cart";
+// import Checkout from "../features/checkout/Checkout";
+// import AdminPage from "../features/admin/AdminPage"; // Import the AdminPage component
+// import {
+//   addToCart,
+//   removeFromCart,
+//   selectCartItems,
+// } from "../features/checkout/cartSlice";
+
+// /**
+//  * COMPONENT
+//  */
+
+// const AppRoutes = () => {
+//   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
+//   const cartItems = useSelector(selectCartItems);
+//   const dispatch = useDispatch();
+//   const isAdmin = useSelector((state) => state.auth.me.isAdmin); // Assuming you have a field for isAdmin in the user object
+//   const navigate = useNavigate(); // Add useNavigate hook to access navigation function
+
+//   useEffect(() => {
+//     dispatch(me());
+//   }, []);
+
+//   return (
+//     <div>
+//       {isLoggedIn ? (
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/home" element={<Home />} />
+//           <Route path="/dogs" element={<AllDogs />} />
+//           <Route path="/dogs/:id" element={<SingleDog />} />
+//           <Route
+//             path="/cart"
+//             element={
+//               <Cart addToCart={addToCart} removeFromCart={removeFromCart} />
+//             }
+//           />
+//           <Route path="/cart/checkout" element={<Checkout />} />
+//           {isAdmin && <Route path="/admin" element={<AdminPage />} />}
+//         </Routes>
+//       ) : (
+//         <Routes>
+//           <Route
+//             path="/"
+//             element={<AuthForm name="login" displayName="Login" />}
+//           />
+//           <Route
+//             path="/login"
+//             element={<AuthForm name="login" displayName="Login" />}
+//           />
+//           <Route
+//             path="/signup"
+//             element={<AuthForm name="signup" displayName="Sign Up" />}
+//           />
+//         </Routes>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default AppRoutes;
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import AuthForm from "../features/auth/AuthForm";
 import Home from "../features/home/Home";
 import { me } from "./store";
@@ -8,6 +78,7 @@ import AllDogs from "../features/allDogs/AllDogs";
 import SingleDog from "../features/singleDog/SingleDog";
 import Cart from "../features/checkout/Cart";
 import Checkout from "../features/checkout/Checkout";
+import AdminPage from "../features/admin/AdminPage"; // Import the AdminPage component
 import {
   addToCart,
   removeFromCart,
@@ -22,153 +93,56 @@ const AppRoutes = () => {
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
   const cartItems = useSelector(selectCartItems);
   const dispatch = useDispatch();
+  const isAdmin = useSelector((state) => state.auth.me.isAdmin);
+  const navigate = useNavigate(); // Add useNavigate hook to access navigation function
 
   useEffect(() => {
     dispatch(me());
   }, []);
 
+  useEffect(() => {
+    if (isAdmin && isLoggedIn) {
+      navigate("/admin"); // Navigate to "/admin" if user is an admin and is logged in
+    }
+  }, [isAdmin, isLoggedIn, navigate]);
+
   return (
     <div>
-      {isLoggedIn ? (
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/dogs" element={<AllDogs />} />
-          <Route path="/dogs/:id" element={<SingleDog />} />
-          <Route
-            path="/cart"
-            element={
-              <Cart addToCart={addToCart} removeFromCart={removeFromCart} />
-            }
-          />
-          <Route path="/cart/checkout" element={<Checkout />} />
-        </Routes>
-      ) : (
-        <Routes>
-          <Route
-            path="/"
-            element={<AuthForm name="login" displayName="Login" />}
-          />
-          <Route
-            path="/login"
-            element={<AuthForm name="login" displayName="Login" />}
-          />
-          <Route
-            path="/signup"
-            element={<AuthForm name="signup" displayName="Sign Up" />}
-          />
-        </Routes>
-      )}
+      <Routes>
+        {isLoggedIn ? (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/dogs" element={<AllDogs />} />
+            <Route path="/dogs/:id" element={<SingleDog />} />
+            <Route
+              path="/cart"
+              element={
+                <Cart addToCart={addToCart} removeFromCart={removeFromCart} />
+              }
+            />
+            <Route path="/cart/checkout" element={<Checkout />} />
+            {isAdmin && <Route path="/admin" element={<AdminPage />} />}
+          </>
+        ) : (
+          <>
+            <Route
+              path="/"
+              element={<AuthForm name="login" displayName="Login" />}
+            />
+            <Route
+              path="/login"
+              element={<AuthForm name="login" displayName="Login" />}
+            />
+            <Route
+              path="/signup"
+              element={<AuthForm name="signup" displayName="Sign Up" />}
+            />
+          </>
+        )}
+      </Routes>
     </div>
   );
 };
 
 export default AppRoutes;
-
-// import React, { useEffect } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { Route, Routes, Navigate } from 'react-router-dom';
-// import AuthForm from '../features/auth/AuthForm';
-// import Home from '../features/home/Home';
-// import { me } from './store';
-// import AllDogs from '../features/allDogs/AllDogs';
-// import SingleDog from '../features/singleDog/SingleDog';
-// import SignUpForm from '../features/auth/SignUpForm';
-// import AdminPage from '../features/admin/AdminPage'; // Import the AdminPage component
-
-// const AppRoutes = () => {
-//   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
-//   const isAdmin = useSelector((state) => state.auth.me.isAdmin); // Assuming you have a field for isAdmin in the user object
-
-//   const dispatch = useDispatch();
-
-//   useEffect(() => {
-//     dispatch(me());
-//   }, []);
-
-//   return (
-//     <div>
-//       {isLoggedIn ? (
-//         <Routes>
-//           {isAdmin ? (
-//             <Route path="/*" element={<AdminPage />} /> // Render AdminPage if user is admin
-//           ) : (
-//             <Navigate to="/home" replace /> // Redirect to home page for non-admin users
-//           )}
-//           <Route path="/home" element={<Home />} />
-//           <Route path="/dogs" element={<AllDogs />} />
-//           <Route path="/dogs/:id" element={<SingleDog />} />
-//         </Routes>
-//       ) : (
-//         <Routes>
-//           <Route
-//             path="/*"
-//             element={<AuthForm name="login" displayName="Login" />}
-//           />
-//           <Route
-//             path="/login"
-//             element={<AuthForm name="login" displayName="Login" />}
-//           />
-//           <Route
-//             path="/signup"
-//             element={<SignUpForm />}
-//           />
-//         </Routes>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default AppRoutes;
-// import React, { useEffect } from "react";
-// import { useSelector, useDispatch } from "react-redux";
-// import { Route, Routes, Navigate } from "react-router-dom";
-// import AuthForm from "../features/auth/AuthForm";
-// import Home from "../features/home/Home";
-// import { me } from "./store";
-// import AllDogs from "../features/allDogs/AllDogs";
-// import SingleDog from "../features/singleDog/SingleDog";
-// import SignUpForm from "../features/auth/SignUpForm";
-// import AdminPage from "../features/admin/AdminPage"; // Import the AdminPage component
-
-// const AppRoutes = () => {
-//   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
-//   const isAdmin = useSelector((state) => state.auth.me.isAdmin); // Assuming you have a field for isAdmin in the user object
-
-//   const dispatch = useDispatch();
-
-//   useEffect(() => {
-//     dispatch(me());
-//   }, []);
-
-//   if (!isLoggedIn) {
-//     return (
-//       <Routes>
-//         <Route
-//           path="/*"
-//           element={<AuthForm name="login" displayName="Login" />}
-//         />
-//         <Route
-//           path="/login"
-//           element={<AuthForm name="login" displayName="Login" />}
-//         />
-//         <Route path="/signup" element={<SignUpForm />} />
-//       </Routes>
-//     );
-//   }
-
-//   return (
-//     <Routes>
-//       {isAdmin ? (
-//         <Route path="/*" element={<AdminPage />} /> // Render AdminPage if user is admin
-//       ) : (
-//         <Route path="/*" element={<Navigate to="/home" replace />} /> // Redirect to home page for non-admin users
-//       )}
-//       <Route path="/home" element={<Home />} />
-//       <Route path="/dogs" element={<AllDogs />} />
-//       <Route path="/dogs/:id" element={<SingleDog />} />
-//     </Routes>
-//   );
-// };
-
-// export default AppRoutes;
